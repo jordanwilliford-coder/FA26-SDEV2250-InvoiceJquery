99fbe88fc24d7491711b8b89e0cf99325a093d8d
jordanwilliford-coder <jordanwilliford@sheridan.edu>
2026-09-08 07:28:00 +0000

    added sheet 1 and totals

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 function processRow(e) {
+  let currentTable = $(e).closest('table');
   let invoiceSubtotal = 0;
 
   $(e).closest('tbody').find('tr').each(function(index) {
-    let qty = parseFloat($(this).find('input.calc[name="quantity"').val());
-    let cost = parseFloat($(this).find('input.calc[name="cost"').val());
-    let subtotal = (qty * cost).toFixed(2);
-    $(this).find('input[name="itemTotal"]').val(subtotal); //Current row total 
-    //Row Caluclations Here
-
-    invoiceSubtotal = invoiceSubtotal + parseFloat(subtotal);
-
+    let qty = parseFloat($(this).find('input.calc[name="quantity"').val()); //get qty
+    let cost = parseFloat($(this).find('input.calc[name="cost"').val()); // get cost
+    
+    let subtotal = (qty * cost).toFixed(2); // row total
+    
+    $(this).find('input[name="itemTotal"]').val(subtotal); //display row total 
+     invoiceSubtotal = invoiceSubtotal + parseFloat(subtotal); // add row total to subtotal
 
   });
-  $('#subTotal').val(invoiceSubtotal);
+  
+  //display subttotal
+  
+  $(currentTable).find('[name="subTotal"]').val(invoiceSubtotal);
+
+
+  let tax = parseFloat($(currentTable).find('[name="tax"]').val()); //get tax
+  let taxAmount = invoiceSubtotal * (tax / 100); //get tax ammount
+
+  $(currentTable).find('[name="taxTotal"]').val(taxAmount); //display tax ammount
+
+
+  let total = invoiceSubtotal + taxAmount; //calcualte total 
+  $(currentTable).find('[name="total"]').val(total); // display the total 
 };
 
 function addRow(e) {
@@ -22,13 +35,13 @@ function addRow(e) {
 };
 
 function main() {
-  // Setup up add row button
-  $('#sheet0 button.addRow').on('click', function(e) {
+  // make selector find sheet class
+  $('.sheet button.addRow').on('click', function(e) {
     addRow(this);
   });
   // find all input elements that will trigger a recalculation of the 
   // sheet.
-  $('#sheet0 input.calc').on('change', function(e) {
+  $('.sheet input.calc').on('change', function(e) {
     processRow(this);
   });
 }
